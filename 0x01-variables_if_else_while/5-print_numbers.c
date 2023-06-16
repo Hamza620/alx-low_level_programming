5d25f77ea8ef19b023646ed3a6ab5c3cf1499939
hamza <hamzahilmi620@gmail.com>
2023-06-16 13:18:35 -0700

    5. Numbers task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (a < 10)
+	{
+		print("%d", n);
+		n++;
+	}
+	putchr('\n');
+	return (0);
+}

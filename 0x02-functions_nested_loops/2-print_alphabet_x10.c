42271ecd1bb619b182c5096679ae9f2fc13c42b2
hamza <hamzahilmi620@gmail.com>
2023-06-20 07:33:35 -0700

    2. 10 x alphabet task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that prints 10 times the alphabet in lowercase
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char l;
+
+	while (i < 10)
+	{
+		for (l = 'a'; l <= 'z'; l++)
+		{
+			_putchar(l);
+		}
+		i++;
+	}
+}

365cbaf08919a1c5a1b94d3da315e62e2bfbb622
hamza <hamzahilmi620@gmail.com>
2023-06-21 15:09:56 -0700

    0. Multiple mains task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point for the program
+ *
+ * Return: Always 0
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (i == 0)
+		printf("%d is zero\n", n);
+	else if (i < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}

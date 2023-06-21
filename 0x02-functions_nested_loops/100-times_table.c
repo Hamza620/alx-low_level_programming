a74b164f17ffcfce1aceea31b84867b08cd4ef74
hamza <hamzahilmi620@gmail.com>
2023-06-20 18:21:47 -0700

    12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,53 +7,95 @@
  */
 void print_times_table(int n)
 {
-	int a, b, c;
-
 	if (n >= 0 && n <= 15)
 	{
+		int a;
+
 		for (a = 0; a <= n; a++)
 		{
-			for (b = 0; b <= n; b++)
-			{
-				c = a * b;
-				if (b != 0)
-				{
-					if (c < 10)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else if (c >= 10 && c < 100)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
-				if (c < 10)
-				{
-					_putchar(c + '0');
-				}
-				else if (c >= 10 && c < 100)
-				{
-					_putchar(c / 10 + '0');
-					_putchar(c % 10 + '0');
-				}
-				else
-				{
-					_putchar(c / 100 + '0');
-					_putchar((c / 10) % 10 + '0');
-					_putchar(c % 10 + '0');
-				}
-			}
-			_putchar('\n');
+			print_row(a, n);
 		}
 	}
 }
+
+/**
+ * print_row - function that prints one row
+ * @r: Row number
+ * @n: Input number
+ */
+void print_row(int r, int n)
+{
+	int b, c;
+
+	for (b = 0; b <= n; b++)
+	{
+		c = r * b;
+		print_number(c);
+		separator(b, c, n);
+	}
+	_putchar('\n');
+}
+
+/**
+ * print_number - function to print a number
+ * @num: Number to be printed
+ */
+void print_number(int num)
+{
+	if (num < 10)
+	{
+		_putchar(num + '0');
+	}
+	else if (num >= 10 && num < 100)
+	{
+		_putchar(num / 10 + '0');
+		_putchar(num % 10 + '0');
+	}
+	else
+	{
+		_putchar(num / 100 + '0');
+		_putchar((num / 10) % 10 + '0');
+		_putchar(num % 10 + '0');
+	}
+}
+
+/**
+ * separator - function that prints the separator between numbers
+ * @b: Current column number
+ * @num: Current number
+ * @n: Input number
+ */
+void separator(int b, int num, int n)
+{
+	if (b != n)
+	{
+		if (num < 10)
+		{
+			print_chars(',', ' ', ' ', ' ');
+		}
+		else if (num >= 10 && num < 100)
+		{
+			print_chars(',', ' ', ' ', ' ');
+		}
+		else
+		{
+			print_chars(',', ' ', ' ');
+		}
+	}
+}
+
+/**
+ * print_chars - function that prints a series of characters
+ * @c1: Character to be printed
+ * @c2: 2nd char
+ * @c3: 3rd char
+ * @c4: 4th char
+ */
+void print_chars(char c1, char c2, char c3, char c4)
+{
+	_putchar(c1);
+	_putchar(c2);
+	_putchar(c3);
+	_putchar(c4);
+}
+

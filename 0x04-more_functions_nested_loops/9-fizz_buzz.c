388d0e5d5ddd3106992aa8f178c6917ce2ae8750
hamza <hamzahilmi620@gmail.com>
2023-06-22 07:22:57 -0700

    9. Fizz-Buzz task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 != 0)
+			printf("Fizz");
+		else if (a % 5 == 0 && a % 3 != 0)
+			printf("Buzz");
+		else if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", a);
+		if (a != 100)
+			printf(" ");
+	}
+	return (0);
+}

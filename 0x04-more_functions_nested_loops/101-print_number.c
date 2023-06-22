2001ffecb2b06f2d72306469c88a67a134ca9f33
hamza <hamzahilmi620@gmail.com>
2023-06-22 14:23:14 -0700

    12. Numbers have life; they're not just symbols on paper task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * powr - calculer the power of two numbers
+ * @a: The base
+ * @b: The exponent
+ * Return: a^b result
+ */
+int powr(int a, int b)
+{
+	int i, p = 1;
+
+	for (i = 0; i < b; i++)
+	{
+		p = p * a;
+	}
+	return (p);
+}
+/**
+ * print_number - function that prints an integer
+ * @n: integer to print
+ *
+ */
+void print_number(int n)
+{
+	int k, tn, digit, c = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	tn = n;
+	while (tn != 0)
+	{
+		tn = tn / 10;
+		c++;
+	}
+	for (k = c; k >= 1; k--)
+	{
+		digit = (n / powr(10, k - 1)) % 10;
+		_putchar(digit + '0');
+	}
+}

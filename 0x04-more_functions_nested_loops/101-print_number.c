907ee1eb6c938a136002ab6b027921947f383f2e
hamza <hamzahilmi620@gmail.com>
2023-06-22 17:46:04 -0700

    test

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,9 +6,9 @@
  * @b: The exponent
  * Return: a^b result
  */
-int powr(int a, int b)
+unsigned int powr(unsigned int a, unsigned int b)
 {
-	int i, p = 1;
+	unsigned int i, p = 1;
 
 	for (i = 0; i < b; i++)
 	{
@@ -22,9 +22,9 @@ int powr(int a, int b)
  * @n: integer to print
  *
  */
-void print_number(int n)
+void print_number(unsigned int n)
 {
-	int k, tn, digit, c = 0;
+	unsigned int k, tn, digit, c = 0;
 
 	if (n < 0)
 	{

e6144c2010dc5a965032ad6fd1d53a8ad236b72d
hamza <hamzahilmi620@gmail.com>
2023-06-26 14:23:35 -0700

    9. strcpy task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies the string pointer
+ * @dest: buffer pointer
+ * @src: string pointer
+ * Return: the pointer dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *d = dest;
+
+	while (*src != '\0')
+	{
+		*d = *src;
+		d++;
+		src++;
+	}
+	*d = '\0';
+	return dest;
+}

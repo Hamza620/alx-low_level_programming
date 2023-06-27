b43edfb34a2a9d4d9fc37fdbe832e343ad2cb77e
hamza <hamzahilmi620@gmail.com>
2023-06-26 17:20:46 -0700

    11. Don't hate the hacker, hate the code task

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry function
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	int s;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		s = s + c;
+		putchar(c);
+	}
+	putchar(2772 - s);
+	return (0);
+}

2079b947fd991ee608a5abe23ce800e7d1f7363a
hamza <hamzahilmi620@gmail.com>
2023-06-26 17:04:45 -0700

    10. Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,16 +1,6 @@
 #include "main.h"
 
 /**
- * _isdigit - to check if it degit
- * @c: degit to test
- * Return: Always  dgits
- */
-int _isdigit(char c)
-{
-	return (c >= '0' && c <= '9');
-}
-
-/**
  * _atoi - function that convert a string to an integer
  * @s: string to convert
  * Return: converted int
@@ -18,36 +8,16 @@ int _isdigit(char c)
 int _atoi(char *s)
 {
 	int g = 1;
-	int r = 0;
-	int i = 0;
+	unsigned int r = 0;
 
-	while (s[i] == ' ')
+	while (*s++)
 	{
-		i++;
-	}
-	if (s[i] == '-')
-	{
-		g = -1;
-		i++;
-	}
-	else if (s[i] == '+')
-	{
-		i++;
-	}
-
-	while (s[i] != '\0')
-	{
-		if (s[i] < '0' || s[i] > '9')
+		if (*s == '-')
+			g = g * -1;
+		else if (*s >= '0' && *s <= '9')
+			r = (r * 10) + (*s - '0');
+		else if (r > 0)
 			break;
-		if (r > 2147483647 /10 || (r == 2147483647 /10 && (s[i] - '0') > 2147483647 % 10 ))
-		{
-			if (g == 1)
-				return (2147483647);
-			else
-				return (-2147483647);
-		}
-		r = (r * 10) + (s[i] - '0');
-		i++;
 	}
 	return (r * g);
 }

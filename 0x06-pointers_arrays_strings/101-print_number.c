1fa777da78d4b720420d21354c7b27f72ac7ed66
hamza <hamzahilmi620@gmail.com>
2023-06-30 10:49:49 -0700

    9. Numbers have life; they're not just symbols on paper task

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ * @n: integer to print
+ *
+ */
+void print_number(int n)
+{
+	unsigned int a;
+
+	if (n < 0)
+	{
+		a = -n;
+		_putchar('-');
+	}
+	else
+	{
+		a = n;
+	}
+	if (a / 10)
+	{
+		print_number(a / 10);
+	}
+}

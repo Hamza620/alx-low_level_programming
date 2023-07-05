1ca89ff61caacba35cef264acfadea168e51dc2e
hamza <hamzahilmi620@gmail.com>
2023-07-05 15:11:46 -0700

    8. Inception. Now, before you bother telling me it's impossible... task

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings 
+ * wildcmp - compares two strings
  * @s1: Input string
  * @s2: Input string
  * Return: 1 if the strings can be considered identical otherwise return 0
@@ -9,20 +9,22 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
 
 	if (*s2 == '*')
 	{
-		if (*(s2 + 1) == '\0' || (*s2 == '*' && *(s2 + 1) == '*'))
-			return (wildcmp(s1, s2 + 1));
-
-		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
-			return (1);
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
-
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
 	return (0);
 }

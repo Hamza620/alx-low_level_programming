d046dc2fd9b7709fd61658b7a634f24b778aedbc
Hamza <hamzahilmi620@gmail.com>
2023-08-01 06:18:28 -0700

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry function
+ * @argc: number of arguments
+ * @argv: Array of pointers to the arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, res;
+	
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	res = op_func(num1, num2);
+	printf("%d\n", res);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - the sum of two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference of two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: a / b or error when b = 0
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of the division of two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: a % b or error when b = 0
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}

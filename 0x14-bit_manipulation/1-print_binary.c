20a856a8945562235a7512bf4bb4ac00bfd25c07
Hamza <hamzahilmi620@gmail.com>
2023-08-31 14:17:36 -0700

    task1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - function that prints the binary representation
+ * of a number
+ * @n: Number to convert
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	int i, size;
+
+	size = 8 * sizeof(n);
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	for (i = size - 1; i >= 0; i--)
+	{
+		if (n & (1L << i))
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+
+}

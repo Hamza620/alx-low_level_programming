02bef54d21bc510c9afb1432d0ea7b7ff96197b1
Hamza <hamzahilmi620@gmail.com>
2023-09-08 13:05:07 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL || file_disc == -1)
 		return (-1);
-	if (strlen(text_content))
+	if (text_content != NULL)
 		wr_res = write(file_disc, text_content, strlen(text_content));
 	close(file_disc);
 	if (wr_res == (ssize_t)strlen(text_content))
